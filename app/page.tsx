f8cf1fabe9babfe46a7dcb4e9bc0d347f1200ee3
tortor1011 <raung1411@gmail.com>
2025-05-02 18:22:01 +0700

    bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
-export default function Home() {
+export default function SimpleContainer() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div>
+      <form action="/test" method="POST">
+        <input type="text" name="firstName" placeholder="Name" required />
+        <input type="text" name="lastName" placeholder="Last name" required /> <br />
+        <input type="email" name="email" placeholder="Email" required /> <br />
+  
+        <label>
+          <input type="checkbox" name="pdpaConsent" required /> Confirm PDPA
+        </label> <br />
+  
+        <div>
+          <label>Gender:</label><br />
+          <label><input type="radio" name="gender" value="male" required /> Male</label>
+          <label><input type="radio" name="gender" value="female" required /> Female</label>
+          <label><input type="radio" name="gender" value="other" required /> Other</label>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  
+        <div>
+          <label>Hobby:</label><br />
+          <label><input type="checkbox" name="hobbies" value="game" /> Game</label>
+          <label><input type="checkbox" name="hobbies" value="music" /> Music</label>
+          <label><input type="checkbox" name="hobbies" value="craft" /> Craft</label>
+          <label><input type="checkbox" name="hobbies" value="reading" /> Reading</label>
+        </div>
+  
+        /* <div>
+          <label htmlFor="status">Status:</label>
+          <select name="status" id="status">
+            <option value="single">Single</option>
+            <option value="married">Married</option>
+            <option value="divorce">Divorced</option>
+          </select>
+          <input type="text" name="Note" placeholder="Note" required />
+        </div> */
+
+        <Autocomplete
+      disablePortal
+       options={['Single', 'Married', 'Divorced']}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Movie" />}
+    />
+
+        
+  
+        <Stack spacing={2} direction="row">
+      <Button variant="contained">Submit</Button>
+    </Stack>
+      </form>
     </div>
   );
+  
 }
