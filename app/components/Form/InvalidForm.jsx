369b32221bcbb08db6efa28d8bed435da81d517c
tortor1011 <raung1411@gmail.com>
2025-05-05 15:24:46 +0700

    ติดบั๊กกรอกได้ทีละตัว

--- app/components/Form/InvalidForm.jsx
+++ app/components/Form/InvalidForm.jsx
@@ -76,12 +76,6 @@ export default function InvalidForm() {
     backgroundColor: '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    ...theme.applyStyles('dark', {
-      backgroundColor: '#1A2027',
-    }),
-    
   }));
   
  
